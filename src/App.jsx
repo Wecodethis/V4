739937b0326cf5eea3eedf496a8150276b6465e4
Wecodethis <wecodethis@gmail.com>
2018-10-21 14:15:36 -0400

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,21 @@
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 class App extends React.Component {
   handleTitleClick() {
     alert("You clicked the title!");
   }
   render() {
-    return React.createElement("div", {}, [
-      React.createElement(
-        "h1",
-        { onClick: this.handleTitleClick },
-        "Adopt Me!"
-      ),
-      React.createElement(Pet, {
-        name: "Luna",
-        Animal: "Dog",
-        breed: "Havenese"
-      }),
-      React.createElement(Pet, {
-        name: "Misu",
-        animal: "Cat",
-        breed: "Street"
-      }),
-      React.createElement(Pet, {
-        name: "Tuck",
-        animal: "Turtle",
-        breed: "Mixed"
-      })
-    ]);
+    return (
+      <div>
+        <h1 onClick={this.handleTitleClick}>Adopt Me!</h1>
+        <Pet name="Luna" animal="Dog" breed="Havanese" />
+        <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+        <Pet name="Doink" animal="Cat" breed="Mixed" />
+      </div>
+    );
   }
 }
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
